7fe3abc6845aa83c5edc768d105c6174c47a77e0
Spibb20 <spibb20@gmail.com>
2026-06-15 17:54:23 +0800

    test pages and components

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,107 @@
+import Link from "next/link";
+import { UserPlus } from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const Employees = [
+  {
+    code: "E-001",
+    name: "Emma Granger",
+    department: "It",
+    position: "Consultant",
+    status: "Active",
+  },
+  {
+    code: "E-002",
+    name: "Emma Granger",
+    department: "It",
+    position: "Consultant",
+    status: "Active",
+  },
+];
+
 export default function DashboardPage() {
-  return <div></div>;
+  return (
+    <div>
+      <section className="flex flex-col justify-between gap-4 sm:flex-row items-center">
+        <div>
+          <p>HR -----</p>
+        </div>
+        <Button asChild className="bg-[#714b67] hover:bg-[#5f3f57]">
+          <Link href="/employees/new">
+            <UserPlus className="mr-2 size-4" />
+            Бүртгэх
+          </Link>
+        </Button>
+      </section>
+      <section className="grid gap-6 ">
+        <Card className="min-w-0">
+          <CardHeader className="flex flex-row items-center justify-between">
+            <div>
+              <CardTitle>Recent Employees</CardTitle>
+              <CardDescription>recent</CardDescription>
+            </div>
+
+            <Button variant="outline" size="sm" asChild>
+              <Link href="/employees">Бүгдийг харах</Link>
+            </Button>
+          </CardHeader>
+
+          <CardContent className="overflow-x-auto">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Код</TableHead>
+                  <TableHead>Ажилтны нэр</TableHead>
+                  <TableHead>Хэлтэс</TableHead>
+                  <TableHead>Албан тушаал</TableHead>
+                  <TableHead>Төлөв</TableHead>
+                </TableRow>
+              </TableHeader>
+
+              <TableBody>
+                {Employees.map((employee) => (
+                  <TableRow key={employee.code}>
+                    <TableCell className="font-medium">
+                      {employee.code}
+                    </TableCell>
+                    <TableCell>{employee.name}</TableCell>
+                    <TableCell>{employee.department}</TableCell>
+                    <TableCell>{employee.position}</TableCell>
+                    <TableCell>
+                      <Badge
+                        variant={
+                          employee.status === "Идэвхтэй"
+                            ? "default"
+                            : "secondary"
+                        }
+                      >
+                        {employee.status}
+                      </Badge>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </section>
+    </div>
+  );
 }
--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,3 +1,58 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  UsersRoundIcon,
+  ChevronsUpDown,
+  LogOut,
+  UserRound,
+} from "lucide-react";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
 export function AppSidebar() {
-  return <div>AppSideBar</div>;
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/dashboard">
+                <div className="flex aspect-square size-9 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
+                  <UsersRoundIcon className="size-5" />
+                </div>
+
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span>HR ERP</span>
+                  <span>Хүний нөөцийн систем</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+    </Sidebar>
+  );
 }
